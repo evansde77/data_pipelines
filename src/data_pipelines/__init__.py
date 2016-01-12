e9d2083eae757b1d00a6c5cce6c662009228e593
Dave Evans <evansde77@gmail.com>
2016-01-12 12:16:37 -0600

    bootstrap package

---
+++ src/data_pipelines/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+"""
+_data_pipelines_ 
+
+Configurable python iterator driven data processing pipelines
+
+"""
+__version__ = '0.0.0'
+__author__ = 'evansde77.github@gmail.com'
